c5259e2bc1aec70e7602d0255929adbf8edceb72
tudor-m <tudor.morosan@yahoo.com>
2017-01-04 08:46:44 -0500

    wip

--- R/CVdata.R
+++ R/CVdata.R
@@ -10,11 +10,13 @@ super_fread <- function( file , key_var=NULL){
 clicks_train  <- super_fread( "../input/clicks_train.csv", key_var = "display_id" )
 clicks_test  <- super_fread( "../input/clicks_test.csv", key_var = "display_id" )
 events  <- super_fread( "../input/events.csv", key_var = "display_id" )
+promoted_content <- super_fread( "../input/promoted_content.csv", key_var = "ad_id")
+documents_meta <- super_fread( "../input/documents_meta.csv", key_var = "document_id")
 
 nrow(clicks_train)
 nrow(clicks_test)
 
-cvmult = 4
+cvmult = 1
 cv_train_size = 1000000*cvmult
 cv_test_size = 380000*cvmult
 set.seed(121)
@@ -25,10 +27,17 @@ s1 = s12[1:cv_train_size]
 s2 = s12[(1+cv_train_size):(cv_train_size+cv_test_size)]
 
 c1 = clicks_train[display_id %in% s1]
+c1_ad = c1$ad_id
 c2 = clicks_train[display_id %in% s2]
+c2_ad = c2$ad_id
 ev = events[display_id %in% s12]
+ev_doc = ev$document_id
+pc = promoted_content[(ad_id %in% c1_ad) | (ad_id %in% c2_ad)]
+dm = documents_meta[document_id %in% ev_doc]
 
 write.csv(c1,file = "../cv/clicks_train.csv",quote = FALSE,row.names = FALSE)
 write.csv(c2,file = "../cv/clicks_test.csv",quote = FALSE,row.names = FALSE)
 write.csv(ev,file = "../cv/events.csv",quote = FALSE,row.names = FALSE)
+write.csv(pc,file = "../cv/promoted_content.csv",quote = FALSE,row.names = FALSE)
+write.csv(dm,file = "../cv/documents_meta.csv",quote = FALSE,row.names = FALSE)
 
--- R/script.R.002.R
+++ R/script.R.002.R
@@ -26,7 +26,7 @@ setkeyv(events,"display_id")
 clicks_train1  <- super_fread( "../cv/clicks_train.csv", key_var = "display_id" )
 clicks_test1   <- super_fread( "../cv/clicks_test.csv" , key_var = "display_id" )
 vTrgt = clicks_test1[clicked==1,2,with=F]
-for (wprob1 in seq(10,24,2)) for (wprob2 in seq(10,24,2)){
+for (wprob1 in seq(10,10,2)) for (wprob2 in seq(10,10,2)){
   print(c(wprob1,wprob2))
 clicks_train <- merge( clicks_train1, events, all.x = F )
 clicks_train[,ad_id_pl:= paste0(ad_id,'_',platform)]
@@ -35,8 +35,8 @@ setkeyv(clicks_train,"ad_id_pl")
 click_prob = clicks_train[,.(sum(clicked)/.N)]
 #wprob = 16
 ad_id_pl_probs   <- get_probs(clicks_train,"ad_id_pl","clicked",wprob1,wprob2)
-rm(clicks_train)
-gc()
+#rm(clicks_train)
+#gc()
 
 clicks_test <- merge( clicks_test1, events, all.x = T )
 
